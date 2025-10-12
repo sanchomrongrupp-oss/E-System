b1b64d9def904c6765477f3eb9de94db8bfe0d17
Rong <haitolanarong007@gmail.com>
2025-10-12 18:28:57 +0700

    update home page

--- app/src/main/java/com/example/e_system/home_page.kt
+++ app/src/main/java/com/example/e_system/home_page.kt
@@ -3,8 +3,7 @@ package com.example.e_system
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class home_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +11,35 @@ class home_page : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
 
+//        val bottomNav = findViewById<BottomNavigationView>(R.id.buttomnavigationbar)
+//
+//        // Load default fragment
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.homeframe, home())
+//            .commit()
+//
+//        bottomNav.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.homeframe, home_page())
+//                        .commit()
+//                    true
+//                }
+//                R.id.exercise -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.container, SearchFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.attendance -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.container, ProfileFragment())
+//                        .commit()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
